e07d1c829b4728d925074f36072ca86617335b45
John Serrano <johnserrano404@gmail.com>
2016-10-01 19:55:50 -0700

    updated documentation in conversions.py. Also, specify python2 in unitconvert

--- conversions.py
+++ conversions.py
@@ -1,12 +1,39 @@
 from unit import Unit
 
 """
-Units are grouped by category. Units can only be converted to other units of the
-same category.
+Each unit is an instance of the Unit class. See unit.py for details.
+For a quick overview, the structure of the entries is:
 
-Base units:
-temperature: fahrenheit
-energy:      joule
+    "$arg_to_match": Unit(
+        "$Name",
+        "$category",
+        $function_to_convert_to_base_units,
+        $function_to_convert_from_base_units,
+    ),
+
+$arg_to_match is the name of the unit as it should be referred to in the
+arguments. This should never be capitalized, but abbreviations might be
+appropriate in some cases.
+
+$Name is the actual name of the unit. This should follow all applicable English
+grammar rules for capitalization.
+
+$category is the conversion group of the unit. For example, all units of energy,
+like joules or calories, should have the "energy" category. Units can only be
+converted to other units of the same category, so spelling and consistent
+capitalization are critical. All categories have a "base unit" which they can
+be converted to and from. The list of all categories and their base unit can be
+found in the table below.
+
+$function_to_convert_to_base_units and $function_to_convert_from_base_units are,
+as you might have guessed, function which convert to and from their category's
+base unit. For style's sake these should be lambdas, unless there is a real need
+for more complicated logic.
+
+Category    | Base unit
+------------------------
+temperature | fahrenheit
+energy      | joule
 """
 
 conversions = {
